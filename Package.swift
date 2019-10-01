909695b964949c6ceccbeb4d42edf18ff49153fc
bugkingK <myway0710@naver.com>
2019-10-01 12:52:47 +0900

    package update

--- Package.swift
+++ Package.swift
@@ -5,4 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "BKCountDownTimer",
+    targets: [
+        .target(
+           name: "BKCountDownTimer",
+           path: "BKCountDownTimer/Sources")
+    ]
 )
